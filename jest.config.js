8103fdae2fcd36860e1603583c21e1356dd612e9
poupicode <gillesmarchesse@gmail.com>
2025-06-27 11:43:40 +0200

    lesgo

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "json-summary"],
   coverageThreshold: {
     global: {
       statements: 80
